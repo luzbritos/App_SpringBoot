3b703ad626225d6dfd4260c4aae2643a7b5a8cc8
lbritos <lbritos@tecnoaccion.com.ar>
2022-11-30 11:33:21 -0300

    Initial commit

---
+++ src/test/java/com/example/App_SpringBoot/AppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.App_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
